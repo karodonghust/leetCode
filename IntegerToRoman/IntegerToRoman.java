1887a0081371c0b62cd95a1e407a1cdc6e7bb8e4
karo <1056926014@qq.com>
2022-11-08 09:53:07 +0800

    some questions

--- IntegerToRoman/IntegerToRoman.java
+++ IntegerToRoman/IntegerToRoman.java
@@ -5,15 +5,21 @@ public class IntegerToRoman {
 
     public static void main(String[] args) {
         IntegerToRoman integerToRoman = new IntegerToRoman();
-        System.out.println(integerToRoman.intToRoman(3));
+        System.out.println(integerToRoman.intToRoman(1994));
     }
 
 
-
-
     public String intToRoman(int num) {
+        int[] number = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String[] label = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
         StringBuilder sb = new StringBuilder();
-
+        for (int i = 0; i < number.length; i++) {
+            int temp = num / number[number.length - 1 - i];
+            for (int j = 0; j < temp; j++) {
+                sb.append(label[number.length - 1 - i]);
+            }
+            num = num % number[number.length - 1 - i];
+        }
 
         return sb.toString();
     }
